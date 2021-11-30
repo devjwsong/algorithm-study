447af7993478dbb57f81a5b67ae71431edbac916
devjwsong <devjwsong@gmail.com>
2021-12-01 00:48:23 +0900

    Added more optimized solution for the problem1.

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-class Solution {
+class Solution1 {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> results(2);
@@ -16,6 +16,37 @@ public:
                     results[1] = j;
 
                     return results;
+                }
+            }
+        }
+
+        return results;
+    }
+};
+
+class Solution2 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> results(2);
+        vector< pair<int, int> > pairs(nums.size());
+
+        for (int i=0; i<nums.size(); ++i) {
+            pairs[i] = pair<int, int> (nums[i], i);
+        }
+
+        sort(pairs.begin(), pairs.end());  // nlogn
+        int i = 0, j = pairs.size()-1;
+        while (j > i) {  // n
+            int sum = pairs[i].first + pairs[j].first;
+            if (sum == target) {
+                results[0] = pairs[i].second;
+                results[1] = pairs[j].second;
+                break;
+            } else {
+                if (sum > target) {
+                    --j;
+                } else {
+                    ++i;
                 }
             }
         }
@@ -46,8 +77,16 @@ int main() {
 
     scanf("%d", &target);
 
-    Solution sol;
-    vector<int> results = sol.twoSum(nums, target);
+    Solution1 sol1;
+    vector<int> results = sol1.twoSum(nums, target);
+
+    for (int i=0; i<results.size(); ++i) {
+        printf("%d ", results[i]);
+    }
+    printf("\n");
+
+    Solution2 sol2;
+    results = sol2.twoSum(nums, target);
 
     for (int i=0; i<results.size(); ++i) {
         printf("%d ", results[i]);
