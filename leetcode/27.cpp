98e598d5da8b157f386fe78105887e770705bd94
devjwsong <devjwsong@gmail.com>
2021-12-22 00:01:07 +0900

    Solved the problem 27, 28 wigh cpp.

---
+++ leetcode/27.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int idx = 0;
+        while (idx < nums.size()) {
+            if (nums[idx] == val) {
+                int dist = 0;
+                for (int i=idx+1; i<nums.size(); ++i) {
+                    if (nums[i] != val) {
+                        dist = i - idx;
+                        break;
+                    }
+                }
+
+                for (int i=idx; i<nums.size(); ++i) {
+                    if (i+dist < nums.size()) {
+                        nums[i] = nums[i+dist];
+                        nums[i+dist] = -1;
+                    } else {
+                        nums[i] = -1;
+                    }
+                }
+            }
+
+            idx += 1;
+        }
+
+        int answer = 0;
+        for (int i=0; i<nums.size(); ++i) {
+            if (nums[i] != -1) {
+                answer += 1;
+            }
+        }
+
+        for (int i=0; i<nums.size(); ++i) {
+            printf("%d ", nums[i]);
+        }
+        printf("\n");
+
+        return answer;
+    }
+};
+
+
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    vector<int> nums;
+    for (int i=0; i<n; ++i) {
+        int e;
+        scanf(" %d", &e);
+        nums.push_back(e);
+    }
+
+    int val;
+    scanf("%d", &val);
+
+    Solution *sol = new Solution();
+    int answer = sol->removeElement(nums, val);
+
+    printf("%d\n", answer);
+
+    return 0;
+}
---
+++ leetcode/28.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> getPi(string needle) {
+        vector<int> pi (needle.size(), 0);
+        int j = 0;
+        for (int i=1; i<needle.size(); ++i) {
+            while (j > 0 && needle[i] != needle[j]) {
+                j = pi[j-1];
+            }
+
+            if (needle[i] == needle[j]) {
+                pi[i] = j+1;
+                ++j;
+            }
+        }
+
+        return pi;
+    }
+
+    int strStr(string haystack, string needle) {
+        if (needle.size() == 0) {
+            return 0;
+        } else if (haystack.size() < needle.size()) {
+            return -1;
+        } else {
+            vector<int> pi = getPi(needle);
+            int cur = 0, matched = 0;
+            while (cur < haystack.size()) {
+                if (haystack[cur+matched] == needle[matched]) {
+                    matched += 1;
+
+                    if (matched == needle.size()) {
+                        return cur;
+                    }
+
+                } else {
+                    if (matched == 0) {
+                        cur += 1;
+                    } else {
+                        cur += (matched - pi[matched-1]);
+                        matched = pi[matched-1];
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+};
+
+
+int main() {
+
+    string haystack;
+    string needle;
+
+    getline(cin, haystack);
+    getline(cin, needle);
+
+    Solution *sol = new Solution();
+    int answer = sol->strStr(haystack, needle);
+
+    printf("%d\n", answer);
+
+    return 0;
+}
