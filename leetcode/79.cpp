07fe99cbfcdbd2c0c822563c322cfebe56213990
devjwsong <devjwsong@gmail.com>
2022-03-19 00:43:45 +0900

    Solved the problem 79 with cpp.

---
+++ leetcode/79.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<bool>> used;
+    int rowDirs[4] = {-1, 1, 0, 0};
+    int colDirs[4] = {0, 0, -1, 1};
+
+    bool search(vector<vector<char>>& board, string word, int idx, int row, int col) {
+        char letter = word[idx];
+        if (board[row][col] == letter && !used[row][col]) {
+            if (idx == (word.size()-1)) {
+                return true;
+            } else {
+                used[row][col] = true;
+                bool res = false;
+                for (int d=0; d<4; ++d) {
+                    int nextRow = row + rowDirs[d], nextCol = col + colDirs[d];
+                    if (nextRow >= 0 && nextRow < board.size() && nextCol >= 0 && nextCol < board[0].size()) {
+                        res = res || search(board, word, idx+1, nextRow, nextCol);
+                    }
+                }
+                used[row][col] = false;
+                return res;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size();
+        for (int r=0; r<m; ++r) {
+            used.push_back(vector<bool> (n, false));
+        }
+
+        if (word.size() > (m*n)) return false;
+
+        for (int r=0; r<m; ++r) {
+            for (int c=0; c<n; ++c) {
+                bool found = search(board, word, 0, r, c);
+                used[r][c] = false;
+                if (found) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    vector<vector<char>> board;
+    for (int r=0; r<m; ++r) {
+        vector<char> row;
+        for (int c=0; c<n; ++c) {
+            char letter;
+            scanf(" %c", &letter);
+            row.push_back(letter);
+        }
+        board.push_back(row);
+    }
+
+    string word;
+    getline(cin >> ws, word);
+
+    Solution *sol = new Solution();
+    bool answer = sol->exist(board, word);
+    printf("%d\n", answer);
+
+    return 0;
+}
