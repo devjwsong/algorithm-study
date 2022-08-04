4118f2ea9116918df84bc59a2a783cf2e7e4513d
devjwsong <devjwsong@gmail.com>
2022-08-04 16:50:07 +0900

    Updated 48.cpp

--- leetcode/48.cpp
+++ leetcode/48.cpp
@@ -8,21 +8,16 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        for (int i=0; i<n/2; ++i) {
-            int numHalf = 0;
-            if (n % 2 == 0) {
-                numHalf = n/2;
-            } else {
-                numHalf = n/2 + 1;
+        for (int start=0; start<n/2; ++start) {
+            int end = n - 1 - start;
+            for (int c=start; c<end; ++c) {
+                int temp = matrix[start][c];
+                matrix[start][c] = matrix[n-1-c][start];
+                matrix[n-1-c][start] = matrix[n-1-start][n-1-c];
+                matrix[n-1-start][n-1-c] = matrix[c][n-1-start];
+                matrix[c][n-1-start] = temp;
             }
-
-            for (int j=0; j<numHalf; ++j) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[n-j-1][i];
-                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
-                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
-                matrix[j][n-i-1] = temp;
-            }
+            
         }
     }
 };
@@ -46,7 +41,8 @@ int main() {
 
     Solution *sol = new Solution();
     sol->rotate(matrix);
-
+    
+    printf("##########\n");
     for (int r=0; r<n; ++r) {
         for (int c=0; c<n; ++c) {
             printf("%d ", matrix[r][c]);
