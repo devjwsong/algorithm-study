bb058e9d5e37787765c0facf9377ca9e3c17ad7b
devjwsong <devjwsong@gmail.com>
2022-08-18 07:33:12 -0400

    Updated 81.cpp.

--- leetcode/81.cpp
+++ leetcode/81.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 /*
-1. Reducing the lenght in the flat case. Reducing both side makes the array converged into strictly increasing condition.
+1. Reducing the length in the flat case. Reducing both side makes the array converged into strictly increasing condition.
 2. I should check what half array contains the target & mid.
 */
 class Solution {
@@ -16,25 +16,26 @@ public:
         int left = 0, right = n-1;
         while(left + 1 < right) {
             int mid = (left + right) / 2;
+            if (nums[left] == target || nums[mid] == target || nums[right] == target) return true;
 
             if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
                 ++left;
                 --right;
             } else if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && nums[mid] > target) {
+                if (target < nums[mid] && target > nums[left]) {
                     right = mid;
                 } else {
                     left = mid;
                 }
             } else {
-                if (nums[right] >= target && nums[mid] < target) {
+                if (target > nums[mid] && target < nums[right]) {
                     left = mid;
                 } else {
                     right = mid;
                 }
             }
         }
-        
+
         if (nums[left] == target || nums[right] == target) return true;
         return false;
     }
