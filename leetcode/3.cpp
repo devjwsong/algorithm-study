93fcda2527d06530f7e4741bb398146924e00cf7
devjwsong <devjwsong@gmail.com>
2022-09-02 22:42:31 -0400

    Updated 3.cpp.

--- leetcode/3.cpp
+++ leetcode/3.cpp
@@ -7,42 +7,31 @@ using namespace std;
 
 /*
 A hash map is used to memorize the last index of each character.
-And curStr is the current longest substring only containing unique letters.
-If s[i] is not in m, then it is unique so there is no problem with including it.
-If s[i] is in m, that means somewhere another same letter already showed up.
-So first we update the answer and get the new starting point.
-If current start is higher than m[s[i]]+1, m[s[i]] will be updated to the new index anyway so we keep the starting position.
-If m[s[i]]+1 is higher, that means m[s[i]] is contained in curStr, 
-so in order to start again we should set the new starting point to m[s[i]]+1.
+Now s[left:right] is the current substring and right indicates the current index.
+If s[right] exists in the hash map, then we should shrink the substring.
+The point here is if current left > mp[s[right]]+1, then left should be kept the same.
+For example, if the input is "abba" and if right = 3,
+then mp['a'] = 0 made the left = 1, which is not what we want.
+So left should be kept as a lowerbound of the sub string.
 */
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if (s.size() == 0) {
-            return 0;
-        }
-
+        unordered_map<char, int> mp;
+        int n = s.size();
+        if (n == 0) return 0;
+        
+        int left = 0, right = 0;
         int answer = 0;
-
-        unordered_map<char, int> m;
-        string curStr = s.substr(0, 1);
-        m[s[0]] = 0;
-        int start = 0;
-        for (int i=1; i<s.size(); ++i) {
-            if (m.find(s[i]) != m.end()) {
-                answer = max(answer, (int) curStr.size());
-                int newStart = max(start, m[s[i]]+1);
-                curStr = s.substr(newStart, i-newStart+1);
-                m[s[i]] = i;
-                start = newStart;
-            } else {
-                curStr += s.substr(i, 1);
-                m[s[i]] = i;
+        while (right < n) {
+            if (mp.find(s[right]) != mp.end()) {
+                left = max(left, mp[s[right]] + 1);
             }
+            mp[s[right]] = right;
+            answer = max(answer, right-left+1);
+            ++right;
         }
-
-        answer = max(answer, (int) curStr.size());
-
+        
         return answer;
     }
 };
