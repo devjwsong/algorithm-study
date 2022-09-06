6e0c5c2a16755779fdd495af7664ad51a187d54f
devjwsong <devjwsong@gmail.com>
2022-09-06 18:35:44 -0400

    Updated 5.cpp.

--- leetcode/5.cpp
+++ leetcode/5.cpp
@@ -4,54 +4,51 @@
 using namespace std;
 
 
-// dp[i][j]: is substring [i:j] palindrome?
+/*
+dp[i][j]: is substring [i:j] palindrome?
+Time: O(n^2)
+Space: O(n^2)
+*/ 
 class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        vector<vector<int>> dp (n, vector<int> (n, 0));  // 0: not palindrome, 1: palindrome
-
-        int maxStart = 0;
-        int maxLen = 0;
+        vector<vector<bool>> dp (n, vector<bool> (n, 0));
+        
+        int start = 0, maxLen = 0;
         for (int dist=0; dist<n; ++dist) {
             for (int i=0; i<n-dist; ++i) {
                 if (dist == 0) {
-                    dp[i][i+dist] = 1;
-                    if (dist+1 >= maxLen) {
-                        maxLen = dist+1;
-                        maxStart = i;
-                    }
-                }
-
-                if (dist == 1) {
+                    dp[i][i+dist] = true;
+                    maxLen = dist + 1;
+                    start = i;
+                } else if (dist == 1) {
                     if (s[i] == s[i+dist]) {
-                        dp[i][i+dist] = 1;
-                        if (dist+1 >= maxLen) {
-                            maxLen = dist+1;
-                            maxStart = i;
+                        dp[i][i+dist] = true;
+                        if (dist + 1 >= maxLen) {
+                            maxLen = dist + 1;
+                            start = i;
                         }
                     } else {
-                        dp[i][i+dist] = 0;
+                        dp[i][i+dist] = false;
                     }
-                }
-
-                if (dist >= 2) {
-                    if (dp[i+1][i+dist-1] == 1) {
+                } else {
+                    if (dp[i+1][i+dist-1]) {
                         if (s[i] == s[i+dist]) {
-                            dp[i][i+dist] = 1;
-                            if (dist+1 >= maxLen) {
-                                maxLen = dist+1;
-                                maxStart = i;
+                            dp[i][i+dist] = true;
+                            if (dist + 1 >= maxLen) {
+                                maxLen = dist + 1;
+                                start = i;
                             }
                         } else {
-                            dp[i][i+dist] = 0;
+                            dp[i][i+dist] = false;
                         }
                     }
                 }
             }
         }
-
-        return s.substr(maxStart, maxLen);
+        
+        return s.substr(start, maxLen);
     }
 };
 
