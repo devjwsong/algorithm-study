44420fe369d6e56ce5c21a670b3e5d6806ba448a
devjwsong <devjwsong@gmail.com>
2023-05-27 22:27:22 -0700

    Solved 1480.cpp.

---
+++ leetcode/1480.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res (n, nums[0]);
+        for (int i=1; i<n; ++i) {
+            res[i] = res[i-1] + nums[i];
+        }
+
+        return res;
+    }
+};
