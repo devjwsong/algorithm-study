c6e2d2812bcc8e26a9b9224993aa79e300be1cfc
devjwsong <devjwsong@gmail.com>
2023-07-15 07:18:54 -0700

    Updated 48.cpp.

--- leetcode/48.cpp
+++ leetcode/48.cpp
@@ -4,51 +4,24 @@
 using namespace std;
 
 
+/*
+We start from the outer layer to inner layers.
+The blocks in the layer l should move by (n-1-l);
+And each group of 4 blocks have a pattern for moving.
+*/
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        for (int start=0; start<n/2; ++start) {
-            int end = n - 1 - start;
-            for (int c=start; c<end; ++c) {
-                int temp = matrix[start][c];
-                matrix[start][c] = matrix[n-1-c][start];
-                matrix[n-1-c][start] = matrix[n-1-start][n-1-c];
-                matrix[n-1-start][n-1-c] = matrix[c][n-1-start];
-                matrix[c][n-1-start] = temp;
+        for (int l=0; l<n/2; ++l) {
+            int k = n - 1 - l;
+            for (int i=l; i<k; ++i) {
+                int temp = matrix[l][i];
+                matrix[l][i] = matrix[n-1-i][l];
+                matrix[n-1-i][l] = matrix[n-1-l][n-1-i];
+                matrix[n-1-l][n-1-i] = matrix[i][n-1-l];
+                matrix[i][n-1-l] = temp;
             }
-            
         }
     }
 };
-
-
-int main() {
-
-    int n;
-    scanf("%d", &n);
-
-    vector<vector<int>> matrix;
-    for (int r=0; r<n; ++r) {
-        vector<int> row;
-        for (int c=0; c<n; ++c) {
-            int val;
-            scanf(" %d", &val);
-            row.push_back(val);
-        }
-        matrix.push_back(row);
-    }
-
-    Solution *sol = new Solution();
-    sol->rotate(matrix);
-    
-    printf("##########\n");
-    for (int r=0; r<n; ++r) {
-        for (int c=0; c<n; ++c) {
-            printf("%d ", matrix[r][c]);
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
