7e21d85feb4e0c44a54ab8f1d309e65473eb0e27
devjwsong <devjwsong@gmail.com>
2023-07-29 19:02:31 -0700

    Revised 179.cpp.

--- leetcode/179.cpp
+++ leetcode/179.cpp
@@ -5,54 +5,35 @@
 using namespace std;
 
 
+/*
+Custom sorting function.
+Time: O(nlogn * logM). (n: the size of array, M: the maximum value.)
+Space: O(n * logM).
+*/
 class Solution {
 public:
     static bool compare(string s1, string s2) {
-        return s1 + s2 > s2 + s1;
+        string cand1 = s1 + s2;
+        string cand2 = s2 + s1;
+
+        return cand1 > cand2;
     }
 
     string largestNumber(vector<int>& nums) {
         int n = nums.size();
-        bool allZeros = true;
-        for (int i=0; i<n; ++i) {
-            if (nums[i] != 0) {
-                allZeros = false;
-                break;
-            }
-        }
-        if (allZeros) return "0";
+        vector<string> strs (n, "");
 
-        vector<string> strs;
         for (int i=0; i<n; ++i) {
-            strs.push_back(to_string(nums[i]));
+            strs[i] = to_string(nums[i]);
         }
+
         sort(strs.begin(), strs.end(), compare);
-
-        string answer = "";
+        string answer;
         for (int i=0; i<n; ++i) {
+            if (answer.size() == 0 && i < n-1 && strs[i] == "0") continue;
             answer += strs[i];
         }
-
+        
         return answer;
     }
 };
-
-
-int main() {
-
-    int n;
-    scanf("%d", &n);
-
-    vector<int> nums;
-    for (int i=0; i<n; ++i) {
-        int val;
-        scanf(" %d", &val);
-        nums.push_back(val);
-    }
-
-    Solution* sol = new Solution();
-    string answer = sol->largestNumber(nums);
-    cout<<answer<<"\n";
-
-    return 0;
-}
