342994ee3383801c60b4720e0d41601c3a1d1987
devjwsong <devjwsong@gmail.com>
2023-08-18 15:42:54 -0400

    Revised 36.cpp.

--- leetcode/36.cpp
+++ leetcode/36.cpp
@@ -1,49 +1,52 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 
+/*
+Hash map.
+Time: O(81).
+Space: O(9).
+*/
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<vector<bool>> rowCheck (9, vector<bool> (9, false));
-        vector<vector<bool>> colCheck (9, vector<bool> (9, false));;
-        vector<vector<bool>> boxCheck (9, vector<bool> (9, false));;
-
+        unordered_set<char> st;
+        
+        // Checking each row.
         for (int r=0; r<9; ++r) {
+            st.clear();
             for (int c=0; c<9; ++c) {
                 if (board[r][c] == '.') continue;
-                int boxIdx = (r / 3) + (c / 3) * 3;
-                if (rowCheck[r][board[r][c]-'1'] || colCheck[c][board[r][c]-'1'] || boxCheck[boxIdx][board[r][c]-'1']) {
-                    return false;
+                if (st.find(board[r][c]) != st.end()) return false;
+                st.insert(board[r][c]);
+            }
+        }
+
+        // Checking each column.
+        for (int c=0; c<9; ++c) {
+            st.clear();
+            for (int r=0; r<9; ++r) {
+                if (board[r][c] == '.') continue;
+                if (st.find(board[r][c]) != st.end()) return false;
+                st.insert(board[r][c]);
+            }
+        }
+
+        // Checking each section.
+        for (int s=0; s<9; ++s) {
+            st.clear();
+            for (int r=(s/3)*3; r<(s/3+1)*3; ++r) {
+                for (int c=(s%3)*3; c<(s%3+1)*3; ++c) {
+                    if (board[r][c] == '.') continue;
+                    if (st.find(board[r][c]) != st.end()) return false;
+                    st.insert(board[r][c]);
                 }
-                rowCheck[r][board[r][c]-'1'] = true;
-                colCheck[c][board[r][c]-'1'] = true;
-                boxCheck[boxIdx][board[r][c]-'1'] = true;
             }
         }
 
         return true;
     }
 };
-
-int main() {
-
-    vector<vector<char>> board;
-    for (int r=0; r<9; ++r) {
-        vector<char> row;
-        for (int c=0; c<9; ++c) {
-            char val;
-            scanf(" %c", &val);
-            row.push_back(val);
-        }
-        board.push_back(row);
-    }
-
-    Solution* sol = new Solution();
-    bool answer = sol->isValidSudoku(board);
-    printf("%d\n", answer);
-
-    return 0;
-}
