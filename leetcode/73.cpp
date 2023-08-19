cdde9c25a6232c28eaccaba0b3096bfaac7e298f
devjwsong <devjwsong@gmail.com>
2023-08-19 16:45:36 -0400

    Revised 73.cpp.

--- leetcode/73.cpp
+++ leetcode/73.cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 
+/*
+Simple trick with matrix!
+We use the first row and column as the marks.
+First, if there is 0 in the first row or column, we set the boolean marks
+since they will eventually become 0s.
+Then, we now iterate the row 1~m-1 and column 1~n-1.
+If there is 0, then we set matrix[r][0] and matrix[0][c] corresponding to the current position into 0.
+Now those row and column should be set to 0 later.
+It doesn't matter whether matrix[r][0] or matrix[0][c] is already 0 since that means that row or column should be 0 eventually.
+After updating the matrix, then we finally update the first row and column according to the boolean flags.
+*/
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -54,33 +65,3 @@ public:
         }
     }
 };
-
-
-int main() {
-    
-    int m, n;
-    scanf("%d %d", &m, &n);
-
-    vector<vector<int>> matrix;
-    for (int r=0; r<m; ++r) {
-        vector<int> row;
-        for (int c=0; c<n; ++c) {
-            int val;
-            scanf(" %d", &val);
-            row.push_back(val);
-        }
-        matrix.push_back(row);
-    }
-
-    Solution* sol = new Solution();
-    sol->setZeroes(matrix);
-
-    for (int r=0; r<m; ++r) {
-        for (int c=0; c<n; ++c) {
-            printf("%d ", matrix[r][c]);
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
