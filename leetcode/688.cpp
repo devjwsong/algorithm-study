21efc178b76f4c9c5896a44508d30aca981a94a3
devjwsong <devjwsong@gmail.com>
2023-09-04 15:21:29 -0400

    Revised 688.cpp.

--- leetcode/688.cpp
+++ leetcode/688.cpp
@@ -5,27 +5,34 @@
 using namespace std;
 
 
+/*
+Dynamic Programming.
+dp[r][c][i]: The probability of surviving at (r,c) after moving i times.
+If the dp array counts the number of visits, it will cause the overflow.
+And note that we need to add dp[r][c][i] / 8.0 because it need to distribute the probability from each 8 cases.
+This would internally include the case where the knight goes out of the board.
+Time: O(n*n*k).
+Space: O(n*n*k).
+*/
 class Solution {
 public:
-    double knightProbability(int n, int k, int row, int column) {
-        if (row == n || column == n) return 0.0;
-        if (k == 0) return 1.0;
+    int rowMoves[8] = {-1, -2, -2, -1, 1, 2, 2, 1};
+    int colMoves[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
 
-        double dp[n][n][k+1];
-        memset(dp, 0.0, sizeof(dp));
+    double knightProbability(int n, int k, int row, int column) {
+        if (k == 0) return  1.0;
+        
+        vector<vector<vector<double>>> dp (n, vector<vector<double>> (n, vector<double> (k+1, 0.0)));
         dp[row][column][0] = 1.0;
         for (int i=0; i<k; ++i) {
             for (int r=0; r<n; ++r) {
                 for (int c=0; c<n; ++c) {
-                    if (dp[r][c][i] > 0) {
-                        if (r-1 >= 0 && c-2 >= 0) dp[r-1][c-2][i+1] += (dp[r][c][i]/8.0);
-                        if (r-2 >= 0 && c-1 >= 0) dp[r-2][c-1][i+1] += (dp[r][c][i]/8.0);
-                        if (r-2 >= 0 && c+1 < n) dp[r-2][c+1][i+1] += (dp[r][c][i]/8.0);
-                        if (r-1 >= 0 && c+2 < n) dp[r-1][c+2][i+1] += (dp[r][c][i]/8.0);
-                        if (r+1 < n && c+2 < n) dp[r+1][c+2][i+1] += (dp[r][c][i]/8.0);
-                        if (r+2 < n && c+1 < n) dp[r+2][c+1][i+1] += (dp[r][c][i]/8.0);
-                        if (r+2 < n && c-1 >= 0) dp[r+2][c-1][i+1] += (dp[r][c][i]/8.0);
-                        if (r+1 < n && c-2 >= 0) dp[r+1][c-2][i+1] += (dp[r][c][i]/8.0);
+                    for (int d=0; d<8; ++d) {
+                        int nextRow = r + rowMoves[d];
+                        int nextCol = c + colMoves[d];
+                        if (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < n) {
+                            dp[nextRow][nextCol][i+1] += (dp[r][c][i]/8.0);
+                        }
                     }
                 }
             }
@@ -40,16 +47,3 @@ public:
         return answer;
     }
 };
-
-
-int main() {
-
-    int n, k, row, column;
-    scanf("%d %d %d %d", &n, &k, &row, &column);
-
-    Solution* sol = new Solution();
-    double answer = sol->knightProbability(n, k, row, column);
-    printf("%lf\n", answer);
-
-    return 0;
-}
